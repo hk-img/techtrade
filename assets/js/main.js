7f53e012d951348c2ded90dc2a185703452db168
komalimg <komalkhambra.img@gmail.com>
2025-02-14 15:31:57 +0530

    contactus career

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,3 +6,23 @@ document.addEventListener("DOMContentLoaded", () => {
     new Swiper(swiperEl, config);
   });
 });
+
+
+let isOpen = true;
+
+function toggleContent() {
+  var content = document.getElementById("jobContent");
+  var iconPath = document.getElementById("iconPath");
+
+  if (isOpen) {
+    content.style.maxHeight = "0px";
+    content.style.opacity = "0";
+    iconPath.setAttribute("d", "M416 208H272V64c0-17.67-14.33-32-32-32h-32c-17.67 0-32 14.33-32 32v144H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h144v144c0 17.67 14.33 32 32 32h32c17.67 0 32-14.33 32-32V304h144c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z"); // Plus icon
+  } else {
+    content.style.maxHeight = "1000px";
+    content.style.opacity = "1";
+    iconPath.setAttribute("d", "M416 208H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h384c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z"); // Minus icon
+  }
+
+  isOpen = !isOpen;
+}
